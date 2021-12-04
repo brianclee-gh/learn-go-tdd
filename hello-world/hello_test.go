dfb0d60259881e8fa255b5d7ea53a373e221e191
Brian <blee0613@gmail.com>
2021-12-04 15:16:18 -0800

    update hello

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -11,13 +11,25 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Sam", "French")
+		want := "Bonjour, Sam"
+		assertCorrectMessage(t, got, want)
+	})
+
 	t.Run("saying Hello, world when empty string is passed", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, world"
 		assertCorrectMessage(t, got, want)
 	})
