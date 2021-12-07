b6a71f25286c06447906e9935e3f78e424dc4e2d
Brian <blee0613@gmail.com>
2021-12-07 10:29:40 -0800

    shapes

---
+++ structs/shapes.go
@@ -0,0 +1,33 @@
+package main
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return (r.Width + r.Height) * 2.0
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2.0
+}
