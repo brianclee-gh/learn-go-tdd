f5098d8da7c7a096ad14915dea0f5fdd984885a8
Brian <blee0613@gmail.com>
2021-12-18 20:54:44 -0800

    http server add post method

---
+++ maps/dictionary.go
@@ -0,0 +1,37 @@
+package main
+
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("word not found")
+var ErrWordExists = errors.New("word already in dictionary")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	d[word] = newDefinition
+	return nil
+}
